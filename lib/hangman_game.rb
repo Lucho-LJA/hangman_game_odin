eb15af698f2483e3464fd4e4f56300ee31342e31
Lucho-LJA <ljallauca.a@gmail.com>
2022-04-18 21:15:31 -0500

    Add file verification

--- lib/hangman_game.rb
+++ lib/hangman_game.rb
@@ -1,22 +1,26 @@
 path_file = "./google-10000-english-no-swears.txt"
 module General_game
     def General_game.new_word(path)
-        random = Random.new
-        random_index = []
-        lines = File.readlines(path)
-        index_length = lines.length
-        value_found = false
         word = "hangman"
-        while value_found == false do
-            random_num = random.rand(index_length)
-            unless random_index.include?(random_num)
-                word = lines[random_num].chomp
-                if word.length < 5 or word.length > 12
-                    random_index.push(random_num)
-                else
-                    value_found = true
+        if File.exist?(path)
+            random = Random.new
+            random_index = []
+            lines = File.readlines(path)
+            index_length = lines.length
+            value_found = false
+            while value_found == false do
+                random_num = random.rand(index_length)
+                unless random_index.include?(random_num)
+                    word = lines[random_num].chomp
+                    if word.length < 5 or word.length > 12
+                        random_index.push(random_num)
+                    else
+                        value_found = true
+                    end
                 end
             end
+        else
+            puts "No load Dictionary. Choose the word by default"
         end
         return word
     end
