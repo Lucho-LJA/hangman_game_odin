f91caba8dcc3c2a35cfdce60be8668f6c5dabc4f
Lucho-LJA <ljallauca.a@gmail.com>
2022-04-18 21:08:58 -0500

    Add new_word method to generate a word (4-12 charts)

---
+++ lib/hangman_game.rb
@@ -0,0 +1,25 @@
+path_file = "./google-10000-english-no-swears.txt"
+module General_game
+    def General_game.new_word(path)
+        random = Random.new
+        random_index = []
+        lines = File.readlines(path)
+        index_length = lines.length
+        value_found = false
+        word = "hangman"
+        while value_found == false do
+            random_num = random.rand(index_length)
+            unless random_index.include?(random_num)
+                word = lines[random_num].chomp
+                if word.length < 5 or word.length > 12
+                    random_index.push(random_num)
+                else
+                    value_found = true
+                end
+            end
+        end
+        return word
+    end
+end
+
+puts General_game.new_word(path_file)
